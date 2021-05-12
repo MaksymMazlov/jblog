d0d73abe9b14e8f7fb6c4e1daa9845e2bf7a7df1
Mazlov Maksym <mazlovmaxim@gmail.com>
2021-05-12 15:22:17 +0300

    JB-7 Prevent user from like the same post multiple times

--- src/main/java/ua/org/jblog/controller/SearchController.java
+++ src/main/java/ua/org/jblog/controller/SearchController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ua.org.jblog.Exception.EmptyOrNullFieldException;
 import ua.org.jblog.domain.Category;
 import ua.org.jblog.dto.PostDto;
 import ua.org.jblog.service.CategoryService;
@@ -22,12 +23,19 @@ public class SearchController
     @GetMapping("/search")
     public String reqSearch(String s, Model model)
     {
-        List<Category> categories = categoryService.getAll();
-        List<PostDto> posts = postService.getAllBySearch(s);
-        model.addAttribute("search_posts_list", posts);
-        model.addAttribute("categoriesToHTML", categories);
-        model.addAttribute("searchName", s);
-        return "searchPage";
-    }
+        try
+        {
+            List<Category> categories = categoryService.getAll();
+            List<PostDto> posts = postService.getAllBySearch(s);
+            model.addAttribute("search_posts_list", posts);
+            model.addAttribute("categoriesToHTML", categories);
+            model.addAttribute("searchName", s);
+            return "searchPage";
+        }
+        catch (EmptyOrNullFieldException e)
+        {
 
+            return "redirect:/";
+        }
+    }
 }
